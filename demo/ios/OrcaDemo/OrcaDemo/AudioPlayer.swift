88f09bafddf3892a719e81b9691136809640cadd
Kwangsoo Yeo <kyeo@picovoice.ai>
2024-02-13 11:43:09 -0800

    update binding + docs

--- demo/ios/OrcaDemo/OrcaDemo/AudioPlayer.swift
+++ demo/ios/OrcaDemo/OrcaDemo/AudioPlayer.swift
@@ -30,75 +30,27 @@ public class AudioPlayerError: LocalizedError {
 }
 
 public class AudioPlayer: NSObject, AVAudioPlayerDelegate {
-    let audioFilePath = "temp.wav"
-    let sampleRate = 16000.0
-
-    var audioFile: URL?
     var player: AVAudioPlayer?
     var onComplete: ((Bool) -> Void)?
 
-    func play(pcm: [Int16]? = nil, onComplete: ((Bool) -> Void)? = nil) throws {
-        let audioDir = try FileManager.default.url(
-                        for: .documentDirectory,
-                        in: .userDomainMask,
-                        appropriateFor: nil,
-                        create: false)
-        audioFile = audioDir.appendingPathComponent(audioFilePath)
+    func play(audioFile: URL, onComplete: ((Bool) -> Void)? = nil) throws {
         self.onComplete = onComplete
-
-        if pcm != nil {
-            try savePCMToAudioFile(pcm: pcm!, sampleRate: sampleRate)
-        }
-        try playAudioFile()
+        try playAudioFile(audioFile: audioFile)
     }
 
     func stop() {
         player?.stop()
     }
 
-    private func savePCMToAudioFile(pcm: [Int16], sampleRate: Double) throws {
-        guard let format = AVAudioFormat(commonFormat: .pcmFormatInt16,
-                                         sampleRate: sampleRate,
-                                         channels: 1,
-                                         interleaved: false) else {
-            throw AudioPlayerError("Failed to get Audio Format")
-        }
-
-        let audioWav = try openOutputWav(fileUrl: audioFile!, format: format)
-        try writePcmToWav(pcm: pcm, format: format, audioWav: audioWav)
-    }
-
-    private func playAudioFile() throws {
-        player = try AVAudioPlayer(contentsOf: audioFile!)
+    private func playAudioFile(audioFile: URL) throws {
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSession.Category.playback)
+        try audioSession.setActive(true)
+        
+        player = try AVAudioPlayer(contentsOf: audioFile)
         player?.delegate = self
+        player?.setVolume(1.0, fadeDuration: 0.05)
         player?.play()
-    }
-
-    private func writePcmToWav(pcm: [Int16], format: AVAudioFormat, audioWav: AVAudioFile?) throws {
-        let writeBuffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: AVAudioFrameCount(pcm.count))!
-        memcpy(writeBuffer.int16ChannelData![0], pcm, pcm.count * 2)
-        writeBuffer.frameLength = UInt32(pcm.count)
-
-        do {
-            try audioWav?.write(from: writeBuffer)
-        } catch {
-            throw AudioPlayerError("\(error.localizedDescription)")
-        }
-    }
-
-    private func openOutputWav(fileUrl: URL, format: AVAudioFormat) throws -> AVAudioFile? {
-        do {
-            if FileManager.default.fileExists(atPath: fileUrl.path) {
-                try FileManager.default.removeItem(at: fileUrl)
-            }
-            return try AVAudioFile(
-                forWriting: fileUrl,
-                settings: format.settings,
-                commonFormat: .pcmFormatInt16,
-                interleaved: true)
-        } catch {
-            throw AudioPlayerError("\(error.localizedDescription)")
-        }
     }
 
     public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
