ee2a9278e5d49c2660fac9edc76af90da0568096
Benjamin <benjamin@picovoice.ai>
2024-05-01 14:45:58 -0700

    smart wait chunk calculation

--- demo/python/demo_util.py
+++ demo/python/demo_util.py
@@ -1,16 +1,138 @@
-import time
+import math
+import threading
+from dataclasses import dataclass
 from queue import Queue
-from typing import Any, Optional, Sequence, Union
+from time import sleep, time
+from typing import (
+    Any,
+    Callable,
+    Optional,
+    Sequence,
+)
 
 import numpy as np
+import pvorca
 from numpy.typing import NDArray
+from pvorca import OrcaInvalidArgumentError
 from sounddevice import OutputStream, query_devices
+
+
+class OrcaThread:
+    NUM_TOKENS_NEXT_PCM = 3
+    PROCESSING_TIME_MULTIPLIER = 3
+
+    @dataclass
+    class OrcaTextElement:
+        text: str
+        flush: bool
+
+    def __init__(
+            self,
+            play_audio_callback: Callable[[Sequence[int]], None],
+            access_key: str,
+            num_tokens_per_second: int,
+            model_path: Optional[str] = None,
+            library_path: Optional[str] = None,
+    ) -> None:
+
+        self._orca = pvorca.create(access_key=access_key, model_path=model_path, library_path=library_path)
+        self._orca_stream = self._orca.open_stream()
+        self._sample_rate = self._orca.sample_rate
+
+        self._play_audio_callback = play_audio_callback
+        self._num_tokens_per_second = num_tokens_per_second
+        assert self._num_tokens_per_second > 0
+
+        self._queue: Queue[Optional[OrcaThread.OrcaTextElement]] = Queue()
+        self._thread = None
+        self._start_thread()
+
+        self._time_first_audio_available = None
+
+        self._pcm_buffer: Queue[Sequence[int]] = Queue()
+
+        self._set_time = False
+
+    def _start_thread(self) -> None:
+        self._thread = threading.Thread(target=self._run)
+        self._thread.start()
+
+    def _close_thread_blocking(self):
+        self._queue.put_nowait(None)
+        self._thread.join()
+
+    def _compute_first_audio_wait_chunks(self, pcm: Sequence[int], processing_time: float) -> int:
+        seconds_audio = len(pcm) / self._sample_rate
+        llm_wait_seconds = self.NUM_TOKENS_NEXT_PCM / self._num_tokens_per_second
+        orca_wait_seconds = self.PROCESSING_TIME_MULTIPLIER * processing_time
+        wait_seconds = max(llm_wait_seconds + orca_wait_seconds - seconds_audio, 0)
+        wait_chunks = math.ceil(wait_seconds)
+        # print(f"|- wait {wait_chunks} chunks|", flush=True, end="")
+        return wait_chunks
+
+    def _run(self) -> None:
+        while True:
+            orca_input = self._queue.get()
+            if orca_input is None:
+                break
+
+            start = time()
+            try:
+                if not orca_input.flush:
+                    pcm = self._orca_stream.synthesize(orca_input.text)
+                else:
+                    pcm = self._orca_stream.flush()
+                # if pcm is not None and self._time_first_audio_available is not None:
+                #     sleep(0.6)
+            except OrcaInvalidArgumentError as e:
+                print(f"Orca could not synthesize text input `{orca_input.text}`: `{e}`")
+                continue
+
+            if pcm is not None:
+                wait_chunks = 0
+                if self._time_first_audio_available is None:
+                    self._time_first_audio_available = time()
+
+                    # sleep(0.2)
+
+                    processing_time = time() - start
+
+                    wait_chunks = self._compute_first_audio_wait_chunks(pcm=pcm, processing_time=processing_time)
+
+                self._pcm_buffer.put_nowait(pcm)
+
+                while not self._pcm_buffer.empty() and wait_chunks == 0:
+                    self._play_audio_callback(self._pcm_buffer.get())
+
+    def synthesize(self, text: str) -> None:
+        self._queue.put_nowait(self.OrcaTextElement(text=text, flush=False))
+
+    def flush(self) -> None:
+        self._queue.put_nowait(self.OrcaTextElement(text="", flush=True))
+        self._close_thread_blocking()
+        self._start_thread()
+
+    def get_time_first_audio_available(self) -> Optional[float]:
+        return self._time_first_audio_available
+
+    def delete(self) -> None:
+        self._close_thread_blocking()
+        self._orca_stream.close()
+        self._orca.delete()
+
+    @property
+    def sample_rate(self) -> int:
+        return self._sample_rate
+
+    @property
+    def version(self) -> str:
+        return self._orca.version
 
 
 class StreamingAudioDevice:
     def __init__(self, device_info: dict) -> None:
         self._device_info = device_info
-        self._queue: Queue[NDArray] = Queue()
+        self._queue: Queue[Sequence[int]] = Queue()
 
         self._buffer = None
         self._stream = None
@@ -35,15 +157,15 @@ class StreamingAudioDevice:
         if self._queue.empty():
             outdata[:] = 0
             return
-        data = self._queue.get()
-        outdata[:, 0] = data
 
-    def play(self, pcm_chunk: Optional[Union[Sequence[int], NDArray]] = None) -> None:
+        pcm = self._queue.get()
+        outdata[:, 0] = pcm
+
+    def play(self, pcm_chunk: Sequence[int]) -> None:
         if self._stream is None:
             raise ValueError("Stream is not started. Call `start` method first.")
 
-        if pcm_chunk is not None and isinstance(pcm_chunk, list):
-            pcm_chunk = np.array(pcm_chunk, dtype=np.int16)
+        pcm_chunk = np.array(pcm_chunk, dtype=np.int16)
 
         if self._buffer is not None:
             if pcm_chunk is not None:
@@ -52,9 +174,6 @@ class StreamingAudioDevice:
                 pcm_chunk = self._buffer
             self._buffer = None
 
-        if pcm_chunk is None:
-            return
-
         length = pcm_chunk.shape[0]
         for index_block in range(0, length, self._blocksize):
             if (length - index_block) < self._blocksize:
@@ -62,11 +181,11 @@ class StreamingAudioDevice:
             else:
                 self._queue.put_nowait(pcm_chunk[index_block: index_block + self._blocksize])
 
-    def wait_and_terminate(self) -> None:
-        self.wait()
+    def flush_and_terminate(self) -> None:
+        self.flush()
         self.terminate()
 
-    def wait(self) -> None:
+    def flush(self) -> None:
         if self._buffer is not None:
             chunk = np.zeros(self._blocksize, dtype=np.int16)
             chunk[:self._buffer.shape[0]] = self._buffer
@@ -74,9 +193,9 @@ class StreamingAudioDevice:
 
         time_interval = self._blocksize / self._sample_rate
         while not self._queue.empty():
-            time.sleep(time_interval)
+            sleep(time_interval)
 
-        time.sleep(time_interval)
+        sleep(time_interval)
 
     def terminate(self) -> None:
         self._stream.stop()
@@ -89,4 +208,5 @@ class StreamingAudioDevice:
 
 __all__ = [
     "StreamingAudioDevice",
+    "OrcaThread",
 ]
--- demo/python/orca_demo_streaming.py
+++ demo/python/orca_demo_streaming.py
@@ -10,71 +10,18 @@
 #
 
 import argparse
-import json
-import os
 import re
-import struct
 import time
-import wave
-from dataclasses import dataclass
-from typing import Optional, Sequence
+import traceback
+from typing import Sequence
 
 import tiktoken
-from pvorca import create, OrcaActivationLimitError
+from pvorca import OrcaActivationLimitError
 
-CUSTOM_PRON_OPENING_MARKER = "{"
-CUSTOM_PRON_CLOSING_MARKER = "}"
-CUSTOM_PRON_SEPARATOR = "|"
+from demo_util import OrcaThread, StreamingAudioDevice
+
 CUSTOM_PRON_PATTERN = r"\{(.*?\|.*?)\}"
 CUSTOM_PRON_PATTERN_NO_WHITESPACE = r"\{(.*?\|.*?)\}(?!\s)"
-
-SIMULATED_TOKENS_PER_SECOND = 10
-
-
-@dataclass
-class TokenMetadata:
-    string: str
-    pcm: Optional[Sequence[int]] = None
-
-
-@dataclass
-class ChunkMetadata:
-    pcm: Sequence[int]
-    audio_seconds: float
-    proc_seconds: float
-
-
-def save_wav(output_path: str, pcm: Sequence[int], sample_rate: int) -> None:
-    with wave.open(output_path, "wb") as output_file:
-        output_file.setnchannels(1)
-        output_file.setsampwidth(2)
-        output_file.setframerate(sample_rate)
-        output_file.writeframes(struct.pack(f"{len(pcm)}h", *pcm))
-
-
-def save_token_metadata(output_folder: str, token_metadata: Sequence[TokenMetadata], sample_rate: int) -> None:
-    output_path = os.path.join(output_folder, "0.wav")
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    json_data = []
-    for i, metadata in enumerate(token_metadata):
-        pcm_path = None
-        if metadata.pcm is not None:
-            pcm_path = os.path.abspath(
-                os.path.join(
-                    os.path.dirname(output_path), 
-                    f"{os.path.basename(output_path)[:-4]}{i}.wav"))
-            save_wav(
-                output_path=pcm_path, 
-                pcm=metadata.pcm, 
-                sample_rate=sample_rate)
-    
-        json_data.append({"string": metadata.string, "pcm_path": pcm_path})
-        
-    with open(os.path.join(os.path.dirname(output_path), "metadata.json"), "w") as json_file:
-        json.dump(json_data, json_file)
-
-    print(f"Metadata saved to `{output_path}`.")
 
 
 def tokenize_text(text: str) -> Sequence[str]:
@@ -110,104 +57,51 @@ def main(args: argparse.Namespace) -> None:
     access_key = args.access_key
     model_path = args.model_path
     library_path = args.library_path
-    output_path = args.output_path
-    text = args.text
-    no_audio = args.no_audio
-    save_metadata_folder = args.save_metadata_folder
+    text = args.text_to_stream
+    tokens_per_second = args.tokens_per_second
 
-    if not output_path.lower().endswith('.wav'):
-        raise ValueError('Given argument --output_path must have WAV file extension')
+    try:
+        audio_device = StreamingAudioDevice.from_default_device()
+    except Exception as e:
+        print(traceback.format_exc())
+        raise RuntimeError(
+            "Failed to initialize audio device. See error details above and make sure "
+            "you have an audio output device connected.")
 
-    orca = create(access_key=access_key, model_path=model_path, library_path=library_path)
-    orca_stream = orca.open_stream()
+    orca = OrcaThread(
+        play_audio_callback=audio_device.play,
+        num_tokens_per_second=tokens_per_second,
+        access_key=access_key,
+        model_path=model_path,
+        library_path=library_path)
+
+    audio_device.start(sample_rate=orca.sample_rate)
 
     try:
         print(f"Orca version: {orca.version}\n")
 
-        audio_device = None
-        if not no_audio:
-            from demo_util import StreamingAudioDevice
-            audio_device = StreamingAudioDevice.from_default_device()
-            audio_device.start(sample_rate=orca.sample_rate)
-
-        pcm_chunks_metadata = []
-        token_metadata = []
-
-        first_audio_seconds = None
-
-        print(f"Simulated text stream ({SIMULATED_TOKENS_PER_SECOND} tokens / second):")
+        print(f"Simulated text stream:")
         tokens = tokenize_text(text=text)
 
-        initial_tic = time.time()
+        time_start_text_stream = time.time()
         for token in tokens:
             print(f"{token}", end="", flush=True)
 
-            tic = time.time()
-            pcm_chunk = orca_stream.synthesize(text=token)
+            orca.synthesize(text=token)
 
-            if pcm_chunk is not None:
-                toc = time.time()
-
-                if first_audio_seconds is None:
-                    first_audio_seconds = time.time() - initial_tic
-
-                if audio_device is not None:
-                    audio_device.play(pcm_chunk)
-
-                pcm_chunk_metadata = ChunkMetadata(
-                    pcm=pcm_chunk,
-                    audio_seconds=len(pcm_chunk) / orca.sample_rate,
-                    proc_seconds=toc - tic)
-                pcm_chunks_metadata.append(pcm_chunk_metadata)
-
-            token_metadata.append(TokenMetadata(string=token, pcm=pcm_chunk))
-
-            time.sleep(1 / SIMULATED_TOKENS_PER_SECOND)
+            time.sleep(1 / tokens_per_second)
 
         print("\n")
 
-        tic = time.time()
-        pcm_chunk = orca_stream.flush()
-        if first_audio_seconds is None:
-            first_audio_seconds = time.time() - initial_tic
+        orca.flush()
 
-        if pcm_chunk is not None:
-            toc = time.time()
+        text_stream_duration_seconds = time.time() - time_start_text_stream
+        first_audio_available_seconds = orca.get_time_first_audio_available() - time_start_text_stream
+        print(f"Text stream duration: {text_stream_duration_seconds:.2f} seconds")
+        print(f"First audio received: {first_audio_available_seconds:.2f} seconds after text stream started\n")
 
-            if audio_device is not None:
-                audio_device.play(pcm_chunk)
-
-            pcm_chunk_metadata = ChunkMetadata(
-                pcm=pcm_chunk,
-                audio_seconds=len(pcm_chunk) / orca.sample_rate,
-                proc_seconds=toc - tic)
-            pcm_chunks_metadata.append(pcm_chunk_metadata)
-
-            token_metadata.append(TokenMetadata(string="", pcm=pcm_chunk))
-
-        pcm = [sample for chunk_metadata in pcm_chunks_metadata for sample in chunk_metadata.pcm]
-
-        save_wav(output_path=output_path, pcm=pcm, sample_rate=orca.sample_rate)
-        print(f"Audio started playing after {first_audio_seconds:.2f} seconds.\n")
-        print("Summary of audio generation:")
-        print(
-            f"Generated {len(pcm_chunks_metadata)} audio chunk{'' if len(pcm_chunks_metadata) == 1 else 's'} in total.")
-
-        for i, chunk_metadata in enumerate(pcm_chunks_metadata):
-            print(
-                f"Audio chunk #{i}: length = {chunk_metadata.audio_seconds:.2f}s, "
-                f"processing time = {chunk_metadata.proc_seconds:.2f}s.")
-        print()
-        print(f"Final audio written to `{output_path}`.")
-
-        if save_metadata_folder:
-            save_token_metadata(
-                output_folder=save_metadata_folder,
-                token_metadata=token_metadata,
-                sample_rate=orca.sample_rate)
-
-        if audio_device is not None:
-            audio_device.wait_and_terminate()
+        print("Finishing playing audio ...")
+        audio_device.flush_and_terminate()
 
     except OrcaActivationLimitError:
         print("AccessKey has reached its processing limit")
@@ -223,16 +117,6 @@ if __name__ == "__main__":
         required=True,
         help='AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)')
     parser.add_argument(
-        '--text',
-        '-t',
-        required=True,
-        help='Text to be synthesized')
-    parser.add_argument(
-        '--output_path',
-        '-o',
-        required=True,
-        help='Absolute path to .wav file where the generated audio will be stored')
-    parser.add_argument(
         '--library_path',
         '-l',
         help='Absolute path to dynamic library. Default: using the library provided by `pvorca`')
@@ -241,12 +125,14 @@ if __name__ == "__main__":
         '-m',
         help='Absolute path to Orca model. Default: using the model provided by `pvorca`')
     parser.add_argument(
-        '--no-audio',
-        action='store_true',
-        help='Do not play audio')
+        '--text-to-stream',
+        '-t',
+        required=True,
+        help='Text to be streamed to Orca')
     parser.add_argument(
-        '--save-metadata-folder',
-        default=None,
-        help='Path to save metadata for creating animations')
+        '--tokens-per-second',
+        type=int,
+        default=10,
+        help='Number of tokens to be streamed per second to Orca, simulating an LLM response.')
 
     main(parser.parse_args())
