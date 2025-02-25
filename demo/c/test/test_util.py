d41cdcf9a1cbcb9cd6813f4e81f20c19b3eb0905
Ian <ian@picovoice.ai>
2025-02-24 23:08:38 -0800

    c demo fix

--- demo/c/test/test_util.py
+++ demo/c/test/test_util.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2024 Picovoice Inc.
+# Copyright 2024-2025 Picovoice Inc.
 #
 # You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 # file accompanying this source.
@@ -17,20 +17,22 @@ from typing import List, Sequence
 
 
 @dataclass
-class TestSentences:
+class SentenceTestData:
+    language: str
+    models: Sequence[str]
+    random_state: int
     text: str
     text_no_punctuation: str
     text_custom_pronunciation: str
-    text_invalid: Sequence[str]
 
 
-def get_test_data() -> TestSentences:
+def get_test_data() -> SentenceTestData:
     data_file_path = os.path.join(os.path.dirname(__file__), "../../../resources/.test/test_data.json")
     with open(data_file_path, encoding="utf8") as data_file:
-        json_test_data = data_file.read()
-    test_data = json.loads(json_test_data)['test_sentences']
-    test_data.pop("text_alignment")
-    return TestSentences(**test_data)
+        test_data = json.loads(data_file.read())
+
+    sentence_tests = [SentenceTestData(**data) for data in test_data['tests']['sentence_tests']]
+    return sentence_tests[0]
 
 
 def get_model_paths() -> List[str]:
