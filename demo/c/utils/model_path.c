ec0f76e01c4473deb960c820498700969f1bed43
Albert Ho <albert@picovoice.ai>
2025-04-17 11:16:57 -0700

    remove regex from c demo

--- demo/c/utils/model_path.c
+++ demo/c/utils/model_path.c
@@ -1,7 +1,6 @@
 #include "model_path.h"
 #include <dirent.h>
 #include <libgen.h>
-#include <regex.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,70 +12,66 @@
 #define MAX_NUM_LANGUAGES 100
 #define LANGUAGE_CODE_LEN 2
 #define NUM_GENDERS 2
+#define MODEL_FILE_PREFIX "orca_params_"
 
-static void get_abs_models_dir(char *models_dir, size_t size) {
+static void get_models_dirpath(char *models_dirpath, size_t size) {
     char file_path[MAX_PATH];
     strncpy(file_path, __FILE__, MAX_PATH);
     file_path[MAX_PATH - 1] = '\0';
     char *current_dir = dirname(file_path);
-    snprintf(models_dir, size, "%s/../../../lib/common", current_dir);
+    snprintf(models_dirpath, size, "%s/../../../lib/common", current_dir);
+}
+
+static bool is_language_exists(int count, char** languages, const char *language) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(languages[i], language) == 0) {
+            return true;
+        }
+    }
+    return false;
 }
 
 char** get_available_languages(int* languages_count) {
     char** languages = malloc(MAX_NUM_LANGUAGES * sizeof(char*));
     if (!languages) {
-        fprintf(stderr, "Failed to get available languages.\n");
+        fprintf(stderr, "Failed to get available languages (out of memory).\n");
         exit(EXIT_FAILURE);
     }
 
-    regex_t regex;
-    if (regcomp(&regex, "^orca_params_([a-z]{2})_(male|female)\\.pv$", REG_EXTENDED)) {
-        fprintf(stderr, "Failed to get available languages.\n");
-        free(languages);
-        exit(EXIT_FAILURE);
-    }
-
-    char models_dir[MAX_PATH];
-    get_abs_models_dir(models_dir, sizeof(models_dir));
-    DIR* dir = opendir(models_dir);
-    if (!dir) {
-        fprintf(stderr, "Failed to get available languages.\n");
-        regfree(&regex);
+    char models_dirpath[MAX_PATH];
+    get_models_dirpath(models_dirpath, sizeof(models_dirpath));
+    DIR* models_dir = opendir(models_dirpath);
+    if (!models_dir) {
+        fprintf(stderr, "Failed to get available languages (unable to open models directory).\n");
         free(languages);
         exit(EXIT_FAILURE);
     }
 
     struct dirent* entry;
-    regmatch_t matches[3]; // whole match, language, gender
     int count = 0;
-    while ((entry = readdir(dir)) != NULL) {
-        if (regexec(&regex, entry->d_name, 3, matches, 0) == 0) {
-            int len = matches[1].rm_eo - matches[1].rm_so;
-            char language[LANGUAGE_CODE_LEN + 1] = {0};
-            strncpy(language, entry->d_name + matches[1].rm_so, len);
-            language[len] = '\0';
 
-            bool is_already_exists = false;
-            for (int i = 0; i < count; i++) {
-                if (strcmp(languages[i], language) == 0) {
-                    is_already_exists = true;
-                    break;
-                }
-            }
+    while ((entry = readdir(models_dir))) {
+        if (strncmp(entry->d_name, MODEL_FILE_PREFIX, strlen(MODEL_FILE_PREFIX)) != 0) {
+            continue;
+        }
 
-            if (is_already_exists == false && count < MAX_NUM_LANGUAGES) {
-                languages[count] = strdup(language);
-                if (!languages[count]) {
-                    fprintf(stderr, "Failed to get available languages.\n");
-                    exit(EXIT_FAILURE);
-                }
-                count++;
+        const char *lang_code_ptr = entry->d_name + strlen(MODEL_FILE_PREFIX);
+        char lang_code[LANGUAGE_CODE_LEN + 1];
+        strncpy(lang_code, lang_code_ptr, LANGUAGE_CODE_LEN);
+        lang_code[LANGUAGE_CODE_LEN] = '\0';
+
+        if (!is_language_exists(count, languages, lang_code)) {
+            if (count < MAX_NUM_LANGUAGES) {
+                languages[count++] = strdup(lang_code);
+            } else {
+                fprintf(stderr, "Failed to get available languages (too many languages).\n");
+                free(languages);
+                exit(EXIT_FAILURE);
             }
         }
     }
 
-    closedir(dir);
-    regfree(&regex);
+    closedir(models_dir);
 
     *languages_count = count;
     return languages;
@@ -86,7 +81,7 @@ char **get_available_genders(int *genders_count) {
     *genders_count = NUM_GENDERS;
     char **genders = malloc(sizeof(char *) * (*genders_count));
     if (!genders) {
-        fprintf(stderr, "Failed to get available genders.\n");
+        fprintf(stderr, "Failed to get available genders (out of memory).\n");
         exit(EXIT_FAILURE);
     }
     genders[0] = strdup("male");
@@ -98,27 +93,27 @@ char *get_model_path(const char *language, const char *gender) {
     char model_name[MAX_MODEL_NAME];
     snprintf(model_name, sizeof(model_name), "orca_params_%s_%s.pv", language, gender);
 
-    char models_dir[MAX_PATH];
-    get_abs_models_dir(models_dir, sizeof(models_dir));
+    char models_dirpath[MAX_PATH];
+    get_models_dirpath(models_dirpath, sizeof(models_dirpath));
 
     char model_path[MAX_PATH + MAX_MODEL_NAME];
-    snprintf(model_path, MAX_PATH + MAX_MODEL_NAME, "%s/%s", models_dir, model_name);
+    snprintf(model_path, sizeof(model_path), "%s/%s", models_dirpath, model_name);
 
     if (access(model_path, F_OK) == 0) {
         return strdup(model_path);
     }
 
-    DIR *dir = opendir(models_dir);
-    if (!dir) {
-        fprintf(stderr, "Failed to get model path.\n");
+    DIR *models_dir = opendir(models_dirpath);
+    if (!models_dir) {
+        fprintf(stderr, "Failed to get model path (unable to open models directory).\n");
         exit(EXIT_FAILURE);
     }
 
     struct dirent *entry;
-    char available_gender[8] = {0};
+    char available_gender[sizeof("female")] = {0};
 
-    while ((entry = readdir(dir)) != NULL) {
-        if (strncmp(entry->d_name, "orca_params_", 12) == 0) {
+    while ((entry = readdir(models_dir)) != NULL) {
+        if (strncmp(entry->d_name, MODEL_FILE_PREFIX, strlen(MODEL_FILE_PREFIX)) == 0) {
             char *dot = strrchr(entry->d_name, '.');
             if (dot) {
                 *dot = '\0';
@@ -130,11 +125,19 @@ char *get_model_path(const char *language, const char *gender) {
             }
         }
     }
-    closedir(dir);
+    closedir(models_dir);
 
-    fprintf(stderr,
-            "Gender '%s' is not available with language '%s'. Please use gender '%s'.\n",
-            gender, language, available_gender);
+    if (available_gender[0] != '\0') {
+        fprintf(
+                stderr,
+                "Gender '%s' is not available with language '%s'. Please use gender '%s'.\n",
+                gender, language, available_gender);
+    } else {
+        fprintf(
+                stderr,
+                "Gender '%s' is not available with language '%s'.\n",
+                gender, language);
+    }
 
-    return NULL;
+    exit(EXIT_FAILURE);
 }
