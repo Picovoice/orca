d2c031cee873bc06e05c1a609445d64813e09a54
Albert Ho <albert@picovoice.ai>
2025-04-29 13:52:00 -0700

    fix lint and spelling

--- demo/ios/OrcaDemo/OrcaDemo/ValidateTextHelper.swift
+++ demo/ios/OrcaDemo/OrcaDemo/ValidateTextHelper.swift
@@ -8,12 +8,12 @@ class ValidateTextHelper {
         for scalar in input.unicodeScalars {
             let codePoint = scalar.value
 
-            let isStandardJamo = 
+            let isStandardJamo =
                 (codePoint >= 0x1100 && codePoint <= 0x11FF) || // Hangul Jamo
                 (codePoint >= 0x3130 && codePoint <= 0x318F) || // Hangul Compatibility Jamo
                 (codePoint >= 0xAC00 && codePoint <= 0xD7AF) || // Hangul Syllables
                 (codePoint >= 0xA960 && codePoint <= 0xA97F) || // Hangul Jamo Extended-A
-                (codePoint >= 0xD7B0 && codePoint <= 0xD7FF);   // Hangul Jamo Extended-B
+                (codePoint >= 0xD7B0 && codePoint <= 0xD7FF)    // Hangul Jamo Extended-B
 
             if !isStandardJamo {
                 invalidChars.unicodeScalars.append(scalar)
