721f163b00270bb2a3f06bae626eb2608a633d41
Gabriel Stang <gabriel@picovoice.ai>
2026-07-10 13:07:45 -0700

    some research

--- script/artifact_copy.py
+++ script/artifact_copy.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 PEER_MODULE_DIR = os.environ.get("PV_PEER_MODULE_DIR", os.path.join(os.path.dirname(__file__), '..', '..'))
 sys.path.append(os.path.join(PEER_MODULE_DIR, 'zoo-dev/script'))
 
-from automation.artifact_copy import copy_artifacts, platform_archs
+from automation.artifact_copy import copy_artifacts, platform_archs, platform_dirs
 from util.build import Platforms
 
 
@@ -23,15 +23,32 @@ def main():
     products = ["orca"]
     try:
         for arch in platform_archs(args.platform):
+            root_dir = os.path.join(os.path.dirname(__file__), "..")
             copy_artifacts(
                 platform_name=args.platform,
-                root_dir=os.path.join(os.path.dirname(__file__), ".."),
+                root_dir=root_dir,
                 arch=arch,
                 products=products,
                 products_java=products,
                 products_node=products,
                 products_binaries=[],
                 use_product_subdir=False)
+            
+            '''
+            _, artifacts_dir, _ = platform_dirs(
+                root_dir,
+                args.platform,
+                arch,
+                use_product_subdir=False)
+
+            # TODO: is this function multiplatform?
+            # TODO: is this function allowed to perform this operation?
+            shutil.copytree(
+                artifacts_dir,
+                os.path.join(artifacts_dir, args.platform),
+                dirs_exist_ok=True)
+            '''
+
     except Exception as e:
         logging.error(e)
         sys.exit(1)
